22de92a4ec1c5bcfca823ad0cec024ab77e56d6b
thisismugil <thinknewbro@gmail.com>
2025-03-23 12:56:34 +0530

    requirements added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
